7975a1e55d75e311fb6e4f9af5aa97268a372c4d
gopinath1998 <34595359+gopinath1998@users.noreply.github.com>
2017-12-16 13:50:55 +0530

    Add files via upload

---
+++ alphabet or not.py
@@ -0,0 +1,9 @@
+num=str(raw_input("enter the character"))
+
+if num in("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p" ,"q","r","s","t","u","v","w","x","y","z"):
+
+   print"the character is alphabet"
+
+else:
+   
+   print"the character is not alphabet"
